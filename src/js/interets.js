d9cc599d2959604d0c2ea088c0c37cabdeebdee0
Dendenzilla <martinadriaensen@gmail.com>
2019-04-25 10:34:49 +0200

    dual work static

--- src/js/interets.js
+++ src/js/interets.js
@@ -1,19 +1,26 @@
-// var winHeight = 500;
-// var prHeight = (($(".pr1").outerHeight(true)) + ($(".pr2").outerHeight(true)) + ($(".pr3").outerHeight(true)) + ($(".pr4").outerHeight(true)) + ($(".pr5").outerHeight(true)) + ($(".pr6").outerHeight(true)));
+// ON MET LA VARIABLE A 0
 var prH = 0;
-$('.panelR').each(function(){ prH+=$(this).outerHeight(true) });
-console.log(prH);
-
+var plH = 0;
 $(document).ready(function () {
-    $("body").height(prH);
-});
 
-window.addEventListener('resize', function (event) {
-    $(".panelR").height($(window).innerHeight());
+// ON LUI AJOUTE KA HAUTEUR DE CHAQUE DIV AYANT LA CLASSE panelR
+$('.panelR').each(function(){ prH+=$(this).outerHeight(true) });
+
+$('.panelL').each(function(){ plH+=$(this).outerHeight(true) });
+
+// ON DONNE AU BODY LA HAUTEUR DE LA SOMME DES panelR
+    $("body").height(prH);
+    console.log(prH);
+    console.log(plH);
 });
+// ON DEPLACE LA COLONNE DE DROITE AU SCROLL
 $(window).on('scroll',function(){
     $(".rightcol").css('bottom',$(window).scrollTop()*-1);
 });
 // $(".rightcol").css('bottom',(800 + ($(window).scrollTop()))*-1);
 // $(".rightcol").css('bottom',$(window).scrollTop()*-1);
 // ('bottom',(-800 + ($(window).scrollTop()*-1));
+
+
+// var winHeight = 500;
+// var prHeight = (($(".pr1").outerHeight(true)) + ($(".pr2").outerHeight(true)) + ($(".pr3").outerHeight(true)) + ($(".pr4").outerHeight(true)) + ($(".pr5").outerHeight(true)) + ($(".pr6").outerHeight(true)));
