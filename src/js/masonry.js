a8c17622ee4a503684dbc0a0cdae15e0f52d67ee
Dendenzilla <martinadriaensen@gmail.com>
2019-04-26 11:10:10 +0200

    publis second mason

--- src/js/masonry.js
+++ src/js/masonry.js
@@ -6,3 +6,11 @@ $('.grid').masonry({
     horizontalOrder: true,
     gutter: 40/3
   });
+
+  $('.gridP').masonry({
+    // options
+    itemSelector: '.grid-item',
+    columnWidth: 340,
+    horizontalOrder: true,
+    gutter: 30
+  });
