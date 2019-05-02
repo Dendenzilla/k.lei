7e77f277f6a46abb8f2b1f8f9c2a4a5ac028b56a
Dendenzilla <martinadriaensen@gmail.com>
2019-05-03 00:47:52 +0200

    la prez demain susu le stress

--- src/js/masonry.js
+++ src/js/masonry.js
@@ -1,4 +1,4 @@
-  $('.grid').masonry({
+var $grid = $('.grid').masonry({
     // options
     columnWidth: '.grid-sizer',
     itemSelector: '.grid-item',
@@ -6,4 +6,23 @@
     gutter: '.gutter-sizer'
   });
 
-  
+  // layout Masonry after each image loads
+$grid.imagesLoaded().progress( function() {
+  $grid.masonry();
+});
+
+
+$('.container').imagesLoaded()
+  .always( function( instance ) {
+    console.log('all images loaded');
+  })
+  .done( function( instance ) {
+    console.log('all images successfully loaded');
+  })
+  .fail( function() {
+    console.log('all images loaded, at least one is broken');
+  })
+  .progress( function( instance, image ) {
+    var result = image.isLoaded ? 'loaded' : 'broken';
+    console.log( 'image is ' + result + ' for ' + image.img.src );
+  });
